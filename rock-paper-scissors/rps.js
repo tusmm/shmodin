9a51595f00b98adcadad89e3653ad96da23aba2b
ryan <r.ong40915@gmail.com>
2023-11-23 18:28:54 -0500

    rps short assignment done

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -9,11 +9,66 @@ function getComputerChoice() {
     }
 }
 
+// return  0 for draw, 1 on player win, 2 on computer win
 function playRound(playerSelection, computerSelection) {
     let playersChoice = playerSelection.toLowerCase()
+    if (playersChoice == "rock") {
+        if (computerSelection == "rock") {
+            console.log("Draw!")
+            return 0
+        } else if (computerSelection == "scissors") {
+            console.log("You win!")
+            return 1
+        } else {
+            console.log("Computer wins!")
+            return 2
+        }
+    } else if (playersChoice == "paper") {
+        if (computerSelection == "paper") {
+            console.log("Draw!")
+            return 0
+        } else if (computerSelection == "rock") {
+            console.log("You win!")
+            return 1
+        } else {
+            console.log("Computer wins!")
+            return 2
+        }
+    } else if (playersChoice == "scissors") {
+        if (computerSelection == "scissors") {
+            console.log("Draw!")
+            return 0
+        } else if (computerSelection == "paper") {
+            console.log("You win!")
+            return 1
+        } else {
+            console.log("Computer wins!")
+            return 2
+        }
+    }
+    console.log("invalid input")
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0
+    let cpuScore = 0
+
+    let rounds = 5
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = prompt("Please enter rock, paper, or scissors: ");
+        const computerSelection = getComputerChoice();
+        let point = playRound(playerSelection, computerSelection)
+        if (point == 0) {
+            console.log("No points")
+        } else if (point == 1) {
+            playerScore += 1
+        } else if (point == 2) {
+            cpuScore += 1
+        }
+    }
+    console.log("Player score: " + playerScore)
+    console.log("Computer score: " + cpuScore)
+}
+
+game()
